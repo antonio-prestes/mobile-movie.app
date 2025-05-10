1fed8f83f22654a4f942fd64e7fba8b862aa5335
antonio-prestes <diegoantoniodasp@hotmail.com>
2025-05-10 20:04:40 -0300

    Add tab navigation and initial screens with design updates
    Introduced tab navigation for Home, Search, Saved, and Profile screens. Added initial components for these screens along with assets like icons, images, and styles. Updated app configuration and constants for improved theming and branding alignment.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
-module.exports = function (api: any){
+module.exports = function (api) {
     api.cache(true);
     return {
-        presets: ['babel-preset-expo', { jsxImportSource: 'nativewind/babel'}]
-    }
-}
+        presets: [
+            ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+            "nativewind/babel",
+        ],
+    };
+};
